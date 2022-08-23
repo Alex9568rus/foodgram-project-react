c87127d44554e6631ee1d0a234cc175ffa9c3780
Alexandr <zhbakov1995@yandex.ru>
2022-08-23 14:03:08 +0300

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -80,10 +80,10 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
-        'NAME': os.getenv('DB_NAME', default='foodgram'),
-        'USER': os.getenv('POSTGRES_USER', default='alex'),
-        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='5698442'),
-        'HOST': os.getenv('DB_HOST', default='localhost'),
+        'NAME': os.getenv('DB_NAME', default='postgres'),
+        'USER': os.getenv('POSTGRES_USER', default='postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='postgres'),
+        'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default='5432')
     }
 }
