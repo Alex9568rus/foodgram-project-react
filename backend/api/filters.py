43fd51683e61d2ee729e936628936597d1a94065
Alexandr <zhbakov1995@yandex.ru>
2022-09-01 14:02:21 +0300

    new day

--- backend/api/filters.py
+++ backend/api/filters.py
@@ -1,15 +1,15 @@
 from django_filters.rest_framework import FilterSet, filters
-from recipes.models import Recipe, Ingredient  # , Tag
+from recipes.models import Recipe, Ingredient, Tag
 # from rest_framework.filters import SearchFilter
 
 
 class RecipeFilter(FilterSet):
-    tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
-    # tags = filters.ModelMultipleChoiceFilter(
-    #     field_name='tags__slug',
-    #     queryset=Tag.objects.all(),
-    #     to_field_name='slug',
-    # )
+    # tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
+    tags = filters.ModelMultipleChoiceFilter(
+        field_name='tags__slug',
+        queryset=Tag.objects.all(),
+        to_field_name='slug',
+    )
     is_favorited = filters.BooleanFilter(method='filter_is_favorited')
     is_in_shopping_cart = filters.BooleanFilter(
         method='filter_is_in_shopping_cart'
