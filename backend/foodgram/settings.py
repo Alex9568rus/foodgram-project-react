e7d5812ce81db956ec830eb21a59e1bd86ec2e28
Alexandr <zhbakov1995@yandex.ru>
2022-10-06 10:25:20 +0300

    last fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -89,7 +89,6 @@ DATABASES = {
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# AUTH_USER_MODEL = 'users.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
